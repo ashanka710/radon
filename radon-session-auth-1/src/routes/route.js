dbeb91798b4ed0c6b2ba30f95a39f02642ad67a7
AshankaD343 <ashankad343@gmail.com>
2022-06-15 08:55:08 +0530

    auth-111

---
+++ radon-session-auth-1/src/routes/route.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const userController = require("../controllers/userController")
+const mw = require("../middleware/jwtMiddleware")
+
+
+router.get("/test-me", function(req, res) {
+    res.send("My first ever api!")
+})
+
+router.post("/users", userController.createUser)
+
+router.post("/login", userController.loginUser)
+
+
+
+//The userId is sent by front end
+// router.get("/users/:userId", userController.getUserData)
+
+// router.put("/users/:userId", userController.updateUser)
+router.get("/users/:userId", mw.jwtMiddleware, userController.getUserData) //get user data Api
+
+router.put("/users/:userId", mw.jwtMiddleware, userController.updateUser) //update User data
+
+router.delete("/user/:userId", mw.jwtMiddleware, userController.deleteUser) //
+
+
+
+module.exports = router;
