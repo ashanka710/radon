e8745876cd1c01de975a967c114a5d420f5f7a4f
AshankaD343 <ashankad343@gmail.com>
2022-06-16 11:26:40 +0530

    auth-2

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://ASHANKA:NHFhGG1ex2JlmPDH@realmcluster.h0odx.mongodb.net/test", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 9000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 9000))
+});
